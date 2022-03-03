157d7b7e3fecc58b7d56069c6367d43190fe92cc
Bhurva Sharma <me@bhurvasharma.com>
2022-03-03 12:54:36 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,6 @@ class HomePageState extends State<HomePage> {
   var url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
-  late PokeHub pokeHub;
-
   @override
   void initState() {
     super.initState();
@@ -31,77 +29,102 @@ class HomePageState extends State<HomePage> {
     fetchData();
   }
 
-  fetchData() async {
+  Future<List<PokeHub>> fetchData() async {
+    PokeHub? pokeHub;
+    List<PokeHub> pokehublist = [];
     var res = await http.get(Uri.parse(
         "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"));
     var decodedJson = jsonDecode(res.body);
+    print(decodedJson);
 
-    pokeHub = PokeHub.fromJson(decodedJson);
-    print(pokeHub.toJson());
-    setState(() {});
+    for (var data in decodedJson["pokemon"] as List) {
+      pokeHub = PokeHub.fromJson(data);
+      pokehublist.add(pokeHub!);
+    }
+
+    return pokehublist;
   }
 
   @override
   Widget build(BuildContext context) {
     var pokeHub;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("PokeApp"),
-        backgroundColor: Colors.cyan,
-      ),
-      body: pokeHub == null
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : GridView.count(
-              crossAxisCount: 2,
-              children: pokeHub.pokemon
-                  .map((poke) => Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => PokeDetail(
-                                          pokemon: poke,
-                                        )));
-                          },
-                          child: Hero(
-                            tag: poke.img,
-                            child: Card(
-                              elevation: 3.0,
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: NetworkImage(poke.img))),
+        appBar: AppBar(
+          title: Text("PokeApp"),
+          backgroundColor: Colors.cyan,
+        ),
+        body: FutureBuilder<List<PokeHub>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                {
+                  return GridView.count(
+                    crossAxisCount: 2,
+                    children: pokeHub.pokemon
+                        .map((poke) => Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => PokeDetail(
+                                                pokemon: poke,
+                                              )));
+                                },
+                                child: Hero(
+                                  tag: poke.img,
+                                  child: Card(
+                                    elevation: 3.0,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Container(
+                                          height: 100,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                  image:
+                                                      NetworkImage(poke.img))),
+                                        ),
+                                        Text(
+                                          poke.name,
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                  Text(
-                                    poke.name,
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                ],
+                                ),
                               ),
-                            ),
-                          ),
-                        ),
-                      ))
-                  .toList(),
-            ),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.cyan,
-        child: Icon(Icons.refresh),
-      ),
-    );
+                            ))
+                        .toList(),
+                  );
+                }
+              case ConnectionState.none:
+                {
+                  return (Center(
+                    child: Text("No data available"),
+                  ));
+                }
+              case ConnectionState.waiting:
+                {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              case ConnectionState.active:
+                // TODO: Handle this case.
+
+                break;
+            }
+            return Center(
+              child: Text(""),
+            );
+          },
+        ));
   }
 }
